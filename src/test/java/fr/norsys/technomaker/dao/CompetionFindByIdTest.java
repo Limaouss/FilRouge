3e40354b339412464841cebbed309848ff706767
akouz mohamed <akouz.mohamed999@gmail.com>
2017-03-08 23:37:50 +0000

    fixing date probelme using localdate

--- src/test/java/fr/norsys/technomaker/dao/CompetionFindByIdTest.java
+++ src/test/java/fr/norsys/technomaker/dao/CompetionFindByIdTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.sql.Date;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,17 +16,24 @@ import fr.norsys.technomaker.model.Competition;
 public class CompetionFindByIdTest {
 
 	private CompetitionDao competitionDao;
-	private Date datedebut;
+	private Date dateDebut;
+	private Date dateFin;
 
 	@Before
 	public void setup() throws Exception {
 		DataSetUnit dataSource = new DataSetUnit();
 		this.competitionDao = new CompetitionDaoImpl(dataSource.getConnection());
+
+		LocalDate localDebut = LocalDate.of(2017, 05, 12);
+		LocalDate localFin = LocalDate.of(2017, 06, 15);
+		this.dateDebut = Date.valueOf(localDebut);
+		this.dateFin = Date.valueOf(localFin);
+
 	}
 
 	@Test
 	public void shouldReturnCompetitionOnewhenOne() throws SQLException {
-		Competition competitionOne = new Competition(1, new Date(2017, 05, 12), new Date(2017, 06, 12));
+		Competition competitionOne = new Competition(1, this.dateDebut, this.dateFin);
 		assertThat(this.competitionDao.findCompetitionById(1)).isEqualTo(competitionOne);
 	}
 }
