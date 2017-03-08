c10d5254fe596ddfa2f4a12c9032d1a17129f224
akouz mohamed <akouz.mohamed999@gmail.com>
2017-03-08 23:42:06 +0000

    fixing poule method test

--- src/test/java/fr/norsys/technomaker/dao/EquipeFindEquipesByPouleTest.java
+++ src/test/java/fr/norsys/technomaker/dao/EquipeFindEquipesByPouleTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.sql.Date;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,7 +23,11 @@ public class EquipeFindEquipesByPouleTest {
 	public void setup() throws Exception {
 		DataSetUnit data = new DataSetUnit();
 		this.equipeDao = new EquipeDaoImpl(data.getConnection());
-		this.competition = new Competition(1, new Date(2017, 05, 12), new Date(2017, 0, 12));
+		LocalDate localDebut = LocalDate.of(2017, 05, 12);
+		LocalDate localFin = LocalDate.of(2017, 06, 15);
+		Date dateDebut = Date.valueOf(localDebut);
+		Date dateFin = Date.valueOf(localFin);
+		this.competition = new Competition(1, dateDebut, dateFin);
 	}
 
 	@Test
